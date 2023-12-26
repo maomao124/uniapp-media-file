90797d11a580c11735e407968f34ae477e6c4245
mao <1296193245@qq.com>
2023-12-26 17:30:32 +0800

    uniapp-media-file

---
+++ pages/index/test1/test1.vue
@@ -0,0 +1,34 @@
+<template>
+	<view>
+		<button type="primary" @click="button1">选择文件</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			button1()
+			{
+				uni.chooseFile({
+					count:50,
+					type:'all',
+					success: (data) => {
+						console.log(data.tempFilePaths);
+					},
+					fail: (err) => {
+						console.log(err);
+					}
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index/test2/test2.vue
@@ -0,0 +1,35 @@
+<template>
+	<view>
+		<button type="primary" @click="button1">选择微信聊天文件</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			button1()
+			{
+				wx.chooseMessageFile({
+					count:50,
+					type:'all',
+					success: (data) => {
+						console.log(data);
+						console.log(data.tempFilePaths);
+					},
+					fail: (err) => {
+						console.log(err);
+					}
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
